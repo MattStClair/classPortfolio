7bfda8ffe4e62418a3119e36229ace172df79b0e
mstclair <stclairm@spu.edu>
2017-08-28 21:10:47 -0700

    handlebars works

--- public/app.js
+++ public/app.js
@@ -1,5 +1,43 @@
 'use strict';
 
+$(function () {
+  // Grab the template script
+  var homeTemplate = $("#home-template").html();
+
+  // Compile the template
+  var theTemplate = Handlebars.compile(homeTemplate);
+
+  // Define our data object
+  var context = {
+    "body": "I am good at tech stuff, it is all I care about. I am good at tech stuff, it is all I care about. I am good at tech stuff, it is all I care about. "
+  };
+
+  // Pass our data to the template
+  var theHomeCompiledHtml = theTemplate(context);
+
+  // Add the compiled html to the page
+  $('.content-placeholder').html(theHomeCompiledHtml);
+});
+
+$(function () {
+  // Grab the template script
+  var aboutTemplate = $("#about-template").html();
+
+  // Compile the template
+  var theOtherTemplate = Handlebars.compile(aboutTemplate);
+
+  // Define our data object
+  var context2 = {
+    "body": "Hello, my name is Matt St.Clair. I am an Air Force veteran and a mental health therapist, transitioning into the web development field. I am finishing a B.S. in computer applications development from Renton Technical College while also taking full stack web development courses at Codefellows code camp in Seattle. I enjoy front end as well as back end work but am especially fascinated with UX design and how it blends with my social sciences background. I also enjoy front-end work because it allows me to collaborate and create with clients"
+  };
+
+  // Pass our data to the template
+  var theCompiledHtml = theOtherTemplate(context2);
+
+  // Add the compiled html to the page
+  $('.about-body').html(theCompiledHtml);
+});
+
 
 
 function clickHome (){
@@ -8,6 +46,7 @@ function clickHome (){
     $('.about-body').hide();
   });
 }
+
 function clickAboutMe (){
 
   $('#about').click(function(){
@@ -28,7 +67,6 @@ function carouselImage(){
 
 
 $(document).ready(function(){
-
   $('.about-body').hide();
   carouselImage();
   clickHome();
